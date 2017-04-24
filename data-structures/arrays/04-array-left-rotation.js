a2954a9db43a93f8a7991af11d6894df8a6a0f6c
Zachary Lester <zachary.greg.lester@gmail.com>
2017-04-23 18:06:36 -0700

    Add array-left-rotation

---
+++ data-structures/arrays/04-array-left-rotation.js
@@ -0,0 +1,13 @@
+function processData(input) {
+    //Enter your code here
+    input = input.split('\n').map((v) => v.split(' ').map(Number));
+
+    const [n, d] = input[0];
+    const arr = input[1];
+    
+    const index = d % n;
+    const back = arr.slice(index);
+    const front = arr.slice(0, index);
+
+    console.log(back.concat(front).join(' '));
+} 
